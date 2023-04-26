6c876f36659bbb3fa933fb27a9fafd9d021c6bc2
Jeronimo3k <129968359+Jeronimo3k@users.noreply.github.com>
2023-04-26 17:05:05 -0300

    Create 1-lead.js

---
+++ 1-lead.js
@@ -0,0 +1,8 @@
+<template>
+	<h1>Lead {primeiroNome} {sobrenome} </h1>
+  
+	<p>Empresa: {empresa}</p>
+	<p>idade: {idade}</p>
+	<p>Classificação: {classificação}</p>
+	<p>Receita Anual: {receitaAnual} </p>
+</template>
